adb39e3ed38dc7571946b42da27676f173c5aa99
JotaveX <jvpiloni@gmail.com>
2026-09-20 18:53:32 -0300

    api url do cloud run

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   // Em desenvolvimento as chamadas passam pelo proxy do ng serve (proxy.conf.json)
-  apiUrl: 'http://localhost:8080/api',
+  apiUrl: 'https://cantina-api-514520320470.southamerica-east1.run.app/api',
 };
